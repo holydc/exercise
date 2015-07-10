8d7ecf3e3745e616c22b031bba04443ae89dd090
holydc <holydc@gmail.com>
2015-07-10 19:56:42 +0800

    Find the first common element among variable amount of containers of arbitrary types.

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,72 +1,80 @@
-#include <memory>
+#include <tuple>
+#include <utility>
 
-template<class Prototype> class Function;
-
-template<class Ret, class ... Args>
-class Function<Ret(Args...)> {
-public:
-  template<class Target>
-  Function(Target target) : target_ptr_(std::make_shared<TargetImpl<Target>>(std::move(target))) {
+template<class ... Iterators>
+struct ReturnValue {
+  ReturnValue(bool res, Iterators... itrs) : result(res), iterators(std::make_tuple(itrs...)) {
   }
 
-  Ret operator()(Args&&... args) {
-    return target_ptr_->invoke(std::forward<Args>(args)...);
-  }
-
-private:
-  struct TargetWrapper {
-    virtual ~TargetWrapper() = default;
-    virtual Ret invoke(Args&&... args) = 0;
-  }; // struct TargetWrapper
-
-  template<class Target>
-  struct TargetImpl : public TargetWrapper {
-    TargetImpl(Target target) : target_(std::move(target)) {
-    }
-
-    Ret invoke(Args&&... args) override {
-      return target_(std::forward<Args>(args)...);
-    }
-
-    Target target_;
-  }; // struct TargetImpl
-
-  std::shared_ptr<TargetWrapper> target_ptr_;
-}; // class Function
-
-#include <iostream>
-#include <string>
-
-struct Foo {
-  int operator()(const std::string &arg, int n) {
-    std::cout << arg << ":" << n << std::endl;
-    return static_cast<int>(arg.size());
-  }
+  bool result;
+  std::tuple<Iterators...> iterators;
 };
 
-std::string bar(int n) {
-  std::string orz("orz"), ret(std::to_string(n));
-  ret.push_back(':');
-  while (n > 0) {
-    ret += orz;
-    --n;
-  }
-  return std::move(ret);
+template<class Type>
+ReturnValue<> find_first_common_impl(const Type &val) {
+  return ReturnValue<>(true);
 }
 
-int main() {
-  Function<int(const std::string&, int)> f = Foo();
-  Function<decltype(bar)> b(bar);
-  std::cout << b(f("HTC", 2498)) << std::endl;
-
-  Function<int(int)> sum = [&] (int n) -> int {
-    if (n > 0) {
-      return n + sum(n - 1);
-    } else {
-      return 0;
+template<class Type, class Iterator, class ... Iterators>
+ReturnValue<Iterator, Iterators...> find_first_common_impl(const Type &val, std::pair<Iterator, Iterator> head, std::pair<Iterators, Iterators> ...tail) {
+  ReturnValue<Iterator, Iterators...> result(false, head.second, tail.second...);
+  while (head.first != head.second) {
+    if (*(head.first) == val) {
+      auto ret = find_first_common_impl(val, tail...);
+      if (ret.result) {
+        result.result = true;
+        result.iterators = std::tuple_cat(std::make_tuple(head.first), ret.iterators);
+      }
+      break;
     }
+    ++(head.first);
+  }
+  return result;
+}
+
+template<class Iterator, class ... Iterators>
+std::tuple<Iterator, Iterators...> find_first_common(std::pair<Iterator, Iterator> head, std::pair<Iterators, Iterators> ...tail) {
+  std::tuple<Iterator, Iterators...> result = std::make_tuple(head.second, tail.second...);
+  while (head.first != head.second) {
+    auto ret = find_first_common_impl(*(head.first), tail...);
+    if (ret.result) {
+      result = std::tuple_cat(std::make_tuple(head.first), ret.iterators);
+      break;
+    }
+    ++(head.first);
+  }
+  return result;
+}
+
+#include <deque>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <vector>
+
+int main() {
+  std::deque<int> d = {
+    3, 2498, 5566
   };
-  std::cout << sum(10) << std::endl;
+  std::list<int> l = {
+    10, 100, 1000, 3
+  };
+  std::vector<int> v = {
+    0, 1, 2, 3, 4, 5
+  };
+
+  auto itrs = find_first_common(std::make_pair(v.begin(), v.end()), std::make_pair(d.begin(), d.end()), std::make_pair(l.begin(), l.end()));
+  std::cout << "v:" << std::endl;
+  std::copy(std::get<0>(itrs), v.end(), std::ostream_iterator<int>(std::cout, ", "));
+  std::cout << std::endl;
+
+  std::cout << "d:" << std::endl;
+  std::copy(std::get<1>(itrs), d.end(), std::ostream_iterator<int>(std::cout, ", "));
+  std::cout << std::endl;
+
+  std::cout << "l:" << std::endl;
+  std::copy(std::get<2>(itrs), l.end(), std::ostream_iterator<int>(std::cout, ", "));
+  std::cout << std::endl;
 
   return 0;
 }
