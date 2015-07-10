85c858f4f389053adfe748025e1b4c4c45343b15
holydc <holydc@gmail.com>
2015-07-10 20:11:09 +0800

    Practice: fork, exec, and pipe.

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,80 +1,105 @@
-#include <tuple>
-#include <utility>
+#include <cstring>
 
-template<class ... Iterators>
-struct ReturnValue {
-  ReturnValue(bool res, Iterators... itrs) : result(res), iterators(std::make_tuple(itrs...)) {
-  }
-
-  bool result;
-  std::tuple<Iterators...> iterators;
-};
-
-template<class Type>
-ReturnValue<> find_first_common_impl(const Type &val) {
-  return ReturnValue<>(true);
-}
-
-template<class Type, class Iterator, class ... Iterators>
-ReturnValue<Iterator, Iterators...> find_first_common_impl(const Type &val, std::pair<Iterator, Iterator> head, std::pair<Iterators, Iterators> ...tail) {
-  ReturnValue<Iterator, Iterators...> result(false, head.second, tail.second...);
-  while (head.first != head.second) {
-    if (*(head.first) == val) {
-      auto ret = find_first_common_impl(val, tail...);
-      if (ret.result) {
-        result.result = true;
-        result.iterators = std::tuple_cat(std::make_tuple(head.first), ret.iterators);
-      }
-      break;
-    }
-    ++(head.first);
-  }
-  return result;
-}
-
-template<class Iterator, class ... Iterators>
-std::tuple<Iterator, Iterators...> find_first_common(std::pair<Iterator, Iterator> head, std::pair<Iterators, Iterators> ...tail) {
-  std::tuple<Iterator, Iterators...> result = std::make_tuple(head.second, tail.second...);
-  while (head.first != head.second) {
-    auto ret = find_first_common_impl(*(head.first), tail...);
-    if (ret.result) {
-      result = std::tuple_cat(std::make_tuple(head.first), ret.iterators);
-      break;
-    }
-    ++(head.first);
-  }
-  return result;
-}
-
-#include <deque>
 #include <iostream>
-#include <iterator>
-#include <list>
+#include <sstream>
+#include <string>
 #include <vector>
 
-int main() {
-  std::deque<int> d = {
-    3, 2498, 5566
-  };
-  std::list<int> l = {
-    10, 100, 1000, 3
-  };
-  std::vector<int> v = {
-    0, 1, 2, 3, 4, 5
-  };
+#include <sys/wait.h>
+#include <unistd.h>
 
-  auto itrs = find_first_common(std::make_pair(v.begin(), v.end()), std::make_pair(d.begin(), d.end()), std::make_pair(l.begin(), l.end()));
-  std::cout << "v:" << std::endl;
-  std::copy(std::get<0>(itrs), v.end(), std::ostream_iterator<int>(std::cout, ", "));
-  std::cout << std::endl;
+namespace {
+const std::string PIPE = "|";
 
-  std::cout << "d:" << std::endl;
-  std::copy(std::get<1>(itrs), d.end(), std::ostream_iterator<int>(std::cout, ", "));
-  std::cout << std::endl;
+enum PipeEnd {
+  END_READ, END_WRITE
+}; // enum PipeEnd
 
-  std::cout << "l:" << std::endl;
-  std::copy(std::get<2>(itrs), l.end(), std::ostream_iterator<int>(std::cout, ", "));
-  std::cout << std::endl;
+void onError(const std::string &func) {
+  std::cerr << func << " failed: " << strerror(errno) << std::endl;
+}
 
-  return 0;
+int exec(int begin, int end, const std::vector<std::string> &argv, int *fd) {
+  int status = 0;
+  pid_t pid = fork();
+  switch (pid) {
+  case -1: // failure
+    onError("fork");
+    return EXIT_FAILURE;
+
+  case 0: { // child
+    std::vector<char*> args(end - begin + 1, nullptr);
+    for (int i = begin; i < end; ++i) {
+      args[i - begin] = const_cast<char*>(argv[i].c_str());
+    }
+
+    // Redirect input and output
+    if (fd[END_READ] >= 0) {
+      dup2(fd[END_READ], STDIN_FILENO);
+      close(fd[END_READ]);
+    }
+    if (fd[END_WRITE] >= 0) {
+      dup2(fd[END_WRITE], STDOUT_FILENO);
+      close(fd[END_WRITE]);
+    }
+
+    execvp(args[0], &args[0]);
+    _exit(EXIT_FAILURE); // there must be something wrong if reach here
+  }
+
+  default: { // parent
+    if ((waitpid(pid, &status, 0) < 0) && (errno != EINTR)) {
+      status = EXIT_FAILURE;
+    }
+    break;
+  }
+  }
+  return status;
+}
+} // namespace
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::cerr << "No arguments" << std::endl;
+    return 0;
+  }
+
+  std::istringstream iss(argv[1]);
+  std::vector<std::string> args;
+  std::string arg;
+  while (iss >> arg) {
+    // TODO Revert escaped shell arguments
+    args.emplace_back(std::move(arg));
+  }
+  argc = static_cast<int>(args.size());
+
+  int fd[2] = {-1, -1};
+  int begin = 0;
+  for (;;) {
+    int end = begin;
+    while ((end < argc) && (PIPE != args[end])) {
+      ++end;
+    }
+    if (end >= argc) {
+      break;
+    }
+    int fd_pipe[2];
+    if (pipe(fd_pipe) < 0) {
+      onError("pipe");
+      return EXIT_FAILURE;
+    }
+    fd[END_WRITE] = fd_pipe[END_WRITE];
+    if (exec(begin, end, args, fd) == EXIT_FAILURE) {
+      onError(args[begin]);
+      return EXIT_FAILURE;
+    }
+    if (fd[END_READ] >= 0) {
+      close(fd[END_READ]);
+    }
+    fd[END_READ] = fd_pipe[END_READ];
+    close(fd[END_WRITE]);
+    fd[END_WRITE] = -1;
+    begin = end + 1;
+  }
+  return exec(begin, argc, args, fd);
 }
