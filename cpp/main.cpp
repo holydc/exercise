215c172e0c948562e33b16ae69d362b3c3814ddc
holydc <holydc@gmail.com>
2015-07-11 21:40:37 +0800

    Template metaprogramming and variadic template.

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,105 +1,93 @@
-#include <cstring>
+// TMP library
+/**
+ * The container holding the number sequence
+ */
+template<int... N> struct Sequence {};
 
+template<class, int> struct Append;
+
+/**
+ * An util class appending a number to the specified sequence
+ */
+template<int... BEGIN, int END>
+struct Append<Sequence<BEGIN...>, END> {
+  typedef Sequence<BEGIN..., END> result_type;
+};
+
+/**
+ * An util class generating a number sequence in [<tt>BEGIN</tt>, <tt>END</tt>]
+ */
+template<int BEGIN, int END>
+struct SequenceGenerator {
+  static_assert(BEGIN <= END, "SequenceGenerator: invalid range");
+  typedef typename Append<typename SequenceGenerator<BEGIN, END - 1>::result_type, END>::result_type result_type;
+};
+
+/**
+ * Specialization of <em>SequenceGenerator</em>, the terminating condition
+ */
+template<int BEGIN>
+struct SequenceGenerator<BEGIN, BEGIN> {
+  typedef Sequence<BEGIN> result_type;
+};
+
+/**
+ * An util class generating a number sequence in [0, <tt>N</tt>)
+ */
+template<int N>
+struct IndexGenerator {
+  typedef typename SequenceGenerator<0, N - 1>::result_type result_type;
+};
+
+template<template<int> class, class> struct Values;
+
+/**
+ * The container holding the calculated values
+ *
+ * @tparam Calculator The specified calculating method
+ * @tparam ValueType The data type of the calculated values
+ * @tparam N The number sequence to be calculated
+ */
+template<template<int> class Calculator, int... N>
+struct Values<Calculator, Sequence<N...>> {
+  /**
+   * The array of the calculated values
+   */
+  static const int values[sizeof...(N)];
+};
+
+template<template<int> class Calculator, int... N>
+const int Values<Calculator, Sequence<N...>>::values[sizeof...(N)] = {
+  Calculator<N>::value...
+};
+
+// Example
 #include <iostream>
-#include <sstream>
-#include <string>
-#include <vector>
 
-#include <sys/wait.h>
-#include <unistd.h>
+template<int N>
+struct Fibonacci {
+  static_assert(N >= 0, "Fibonacci: invalid argument");
+  static constexpr int value = Fibonacci<N - 2>::value + Fibonacci<N - 1>::value;
+};
 
-namespace {
-const std::string PIPE = "|";
+template<>
+struct Fibonacci<1> {
+  static constexpr int value = 1;
+};
 
-enum PipeEnd {
-  END_READ, END_WRITE
-}; // enum PipeEnd
+template<>
+struct Fibonacci<0> {
+  static constexpr int value = 0;
+};
 
-void onError(const std::string &func) {
-  std::cerr << func << " failed: " << strerror(errno) << std::endl;
-}
+template<int N>
+using FibonacciSequence = Values<Fibonacci, typename IndexGenerator<N>::result_type>;
 
-int exec(int begin, int end, const std::vector<std::string> &argv, int *fd) {
-  int status = 0;
-  pid_t pid = fork();
-  switch (pid) {
-  case -1: // failure
-    onError("fork");
-    return EXIT_FAILURE;
-
-  case 0: { // child
-    std::vector<char*> args(end - begin + 1, nullptr);
-    for (int i = begin; i < end; ++i) {
-      args[i - begin] = const_cast<char*>(argv[i].c_str());
-    }
-
-    // Redirect input and output
-    if (fd[END_READ] >= 0) {
-      dup2(fd[END_READ], STDIN_FILENO);
-      close(fd[END_READ]);
-    }
-    if (fd[END_WRITE] >= 0) {
-      dup2(fd[END_WRITE], STDOUT_FILENO);
-      close(fd[END_WRITE]);
-    }
-
-    execvp(args[0], &args[0]);
-    _exit(EXIT_FAILURE); // there must be something wrong if reach here
+int main() {
+  constexpr int N = 20;
+  for (int i = 0; i < N; ++i) {
+    std::cout << FibonacciSequence<N>::values[i] << ", ";
   }
-
-  default: { // parent
-    if ((waitpid(pid, &status, 0) < 0) && (errno != EINTR)) {
-      status = EXIT_FAILURE;
-    }
-    break;
-  }
-  }
-  return status;
-}
-} // namespace
-
-int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    std::cerr << "No arguments" << std::endl;
-    return 0;
-  }
-
-  std::istringstream iss(argv[1]);
-  std::vector<std::string> args;
-  std::string arg;
-  while (iss >> arg) {
-    // TODO Revert escaped shell arguments
-    args.emplace_back(std::move(arg));
-  }
-  argc = static_cast<int>(args.size());
-
-  int fd[2] = {-1, -1};
-  int begin = 0;
-  for (;;) {
-    int end = begin;
-    while ((end < argc) && (PIPE != args[end])) {
-      ++end;
-    }
-    if (end >= argc) {
-      break;
-    }
-    int fd_pipe[2];
-    if (pipe(fd_pipe) < 0) {
-      onError("pipe");
-      return EXIT_FAILURE;
-    }
-    fd[END_WRITE] = fd_pipe[END_WRITE];
-    if (exec(begin, end, args, fd) == EXIT_FAILURE) {
-      onError(args[begin]);
-      return EXIT_FAILURE;
-    }
-    if (fd[END_READ] >= 0) {
-      close(fd[END_READ]);
-    }
-    fd[END_READ] = fd_pipe[END_READ];
-    close(fd[END_WRITE]);
-    fd[END_WRITE] = -1;
-    begin = end + 1;
-  }
-  return exec(begin, argc, args, fd);
+  std::cout << std::endl;
+  return 0;
 }
