864997bb0bba811677a72891c1d07689e9a9a0af
holydc <holydc@gmail.com>
2015-10-18 16:39:46 +0800

    Accessing variadic variables.

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,83 +1,65 @@
-#include <memory>
+void create_thread(void(*thread_proc)(void*), void *arg) {
+  thread_proc(arg);
+}
 
-template<class Prototype> class Function;
+template<int... N>
+struct Sequence {
+};
 
-template<class Ret, class... Args>
-class Function<Ret(Args...)> {
+template<class, int> struct Append;
+
+template<int... BEGIN, int END>
+struct Append<Sequence<BEGIN...>, END> {
+  typedef Sequence<BEGIN..., END> result_type;
+};
+
+template<int BEGIN, int END>
+struct SequenceGenerator {
+  typedef typename Append<typename SequenceGenerator<BEGIN, END - 1>::result_type, END>::result_type result_type;
+};
+
+template<int BEGIN>
+struct SequenceGenerator<BEGIN, BEGIN> {
+  typedef Sequence<BEGIN> result_type;
+};
+
+#include <tuple>
+#include <type_traits>
+#include <utility>
+
+class Thread {
 public:
-  template<class Target>
-  Function(Target target) : target_(std::make_shared<GeneralTarget<Target>>(std::move(target))) {
-  }
-
-  template<class Type>
-  Function(Type *instance, Ret(Type::*method)(Args...)) : target_(std::make_shared<BindTarget<Type>>(instance, method)) {
-  }
-
-  Ret operator()(Args... args) {
-    return target_->invoke(std::forward<Args>(args)...);
+  template<class Function, class... Args>
+  explicit Thread(Function &&f, Args&&... args) {
+    std::tuple<typename std::decay<Function>::type, typename std::decay<Args>::type...> t(
+      std::forward<Function>(f),
+      std::forward<Args>(args)...);
+    create_thread(
+      ThreadHelper<typename std::decay<Function>::type, typename std::decay<Args>::type...>::thread_proc,
+      &t);
   }
 
 private:
-  struct TargetWrapper {
-    virtual ~TargetWrapper() = default;
-    virtual Ret invoke(Args... args) = 0;
-  }; // struct TargetWrapper
-
-  template<class Target>
-  struct GeneralTarget : public TargetWrapper {
-    GeneralTarget(Target target) : target_(std::move(target)) {
+  template<class Function, class... Args>
+  struct ThreadHelper {
+    static void thread_proc(void *arg) {
+      helper(arg, typename SequenceGenerator<1, sizeof...(Args)>::result_type());
     }
 
-    Ret invoke(Args... args) override {
-      return target_(std::forward<Args>(args)...);
+    template<int... N>
+    static void helper(void *arg, Sequence<N...>&&) {
+      auto tuple = reinterpret_cast<std::tuple<Function, Args...>*>(arg);
+      std::get<0>(*tuple)(std::get<N>(*tuple)...);
     }
-
-    Target target_;
-  }; // struct GeneralTarget
-
-  template<class Type>
-  struct BindTarget : public TargetWrapper {
-    BindTarget(Type *instance, Ret(Type::*method)(Args...)) : instance_(instance), method_(method) {
-    }
-
-    Ret invoke(Args... args) override {
-      return (instance_->*method_)(std::forward<Args>(args)...);
-    }
-
-    Type *instance_;
-    Ret(Type::*method_)(Args...);
-  }; // struct BindTarget
-
-  std::shared_ptr<TargetWrapper> target_;
-}; // class Function
-
-template<class Type, class Ret, class... Args>
-Function<Ret(Args...)> operator->*(std::shared_ptr<Type> instance, Ret(Type::*method)(Args...)) {
-  return Function<Ret(Args...)>(instance.get(), method);
-}
-
-#include <iostream>
-
-struct Foo {
-  Foo(int n) : n_(n) {
-  }
-
-  void method() {
-    std::cout << "Hello, World! " << n_ << std::endl;
-    ++n_;
-  }
-
-  int n_;
+  };
 };
 
+#include <iostream>
+#include <string>
+
 int main() {
-  std::shared_ptr<Foo> foo = std::make_shared<Foo>(5566);
-  (foo->*(&Foo::method))();
-
-  auto method = &Foo::method;
-  (foo->*method)();
-
-  auto func = foo->*method;
-  func();
+  Thread t([] (int a, const std::string &b, bool c) {
+    std::cout << "a:" << a << " b:" << b << " c:" << std::boolalpha << c << std::endl;
+  }, 5566, "Hello, World!", false);
   return 0;
 }
