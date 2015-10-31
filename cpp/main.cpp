61363187280265b5f9e05b7f2ff9ac6f77ec5a3b
holydc <holydc@gmail.com>
2015-10-31 21:25:24 +0800

    Binding class methods: alternative

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,65 +1,129 @@
-void create_thread(void(*thread_proc)(void*), void *arg) {
-  thread_proc(arg);
-}
-
-template<int... N>
-struct Sequence {
-};
-
-template<class, int> struct Append;
-
-template<int... BEGIN, int END>
-struct Append<Sequence<BEGIN...>, END> {
-  typedef Sequence<BEGIN..., END> result_type;
-};
-
-template<int BEGIN, int END>
-struct SequenceGenerator {
-  typedef typename Append<typename SequenceGenerator<BEGIN, END - 1>::result_type, END>::result_type result_type;
-};
-
-template<int BEGIN>
-struct SequenceGenerator<BEGIN, BEGIN> {
-  typedef Sequence<BEGIN> result_type;
-};
-
-#include <tuple>
-#include <type_traits>
+#include <memory>
 #include <utility>
 
-class Thread {
+template<class> class Function;
+
+template<class Ret, class... Args>
+class Function<Ret(Args...)> {
 public:
-  template<class Function, class... Args>
-  explicit Thread(Function &&f, Args&&... args) {
-    std::tuple<typename std::decay<Function>::type, typename std::decay<Args>::type...> t(
-      std::forward<Function>(f),
-      std::forward<Args>(args)...);
-    create_thread(
-      ThreadHelper<typename std::decay<Function>::type, typename std::decay<Args>::type...>::thread_proc,
-      &t);
+  template<class Callable>
+  Function(Callable callable) : impl_(std::make_shared<Impl<Callable>>(std::move(callable))) {
+  }
+
+  Ret operator()(Args... args) {
+    return impl_->invoke(std::forward<Args>(args)...);
   }
 
 private:
-  template<class Function, class... Args>
-  struct ThreadHelper {
-    static void thread_proc(void *arg) {
-      helper(arg, typename SequenceGenerator<1, sizeof...(Args)>::result_type());
+  struct ImplBase {
+    virtual ~ImplBase() = default;
+    virtual Ret invoke(Args... args) = 0;
+  }; // struct ImplBase
+
+  template<class Callable>
+  struct Impl : public ImplBase {
+    Impl(Callable callable) : callable_(std::move(callable)) {
     }
 
-    template<int... N>
-    static void helper(void *arg, Sequence<N...>&&) {
-      auto tuple = reinterpret_cast<std::tuple<Function, Args...>*>(arg);
-      std::get<0>(*tuple)(std::get<N>(*tuple)...);
+    Ret invoke(Args... args) override {
+      return callable_(std::forward<Args>(args)...);
     }
-  };
-};
+
+    Callable callable_;
+  }; // struct Impl
+
+  template<class Method>
+  struct ClassMethodImpl : public ImplBase {
+    ClassMethodImpl(Method method) : method_(method) {
+    }
+
+    Ret invoke(Args... args) override {
+      return bind(std::forward<Args>(args)...);
+    }
+
+    template<class Type, class... _Args>
+    Ret bind(Type *_this, _Args&&... args) {
+      return (_this->*method_)(std::forward<_Args>(args)...);
+    }
+
+    template<class Type, class... _Args>
+    Ret bind(Type &_this, _Args&&... args) {
+      return (_this.*method_)(std::forward<_Args>(args)...);
+    }
+
+    Method method_;
+  }; // struct ClassMethodImpl
+
+  template<class Type, class... _Args>
+  struct Impl<Ret(Type::*)(_Args...)> : public ClassMethodImpl<Ret(Type::*)(_Args...)> {
+    typedef Ret(Type::*Method)(_Args...);
+
+    Impl(Method method) : ClassMethodImpl<Method>(std::move(method)) {
+    }
+  }; // struct Impl<Ret(Type::*)(_Args...)>
+
+  template<class Type, class... _Args>
+  struct Impl<Ret(Type::*)(_Args...) const> : public ClassMethodImpl<Ret(Type::*)(_Args...) const> {
+    typedef Ret(Type::*Method)(_Args...) const;
+
+    Impl(Method method) : ClassMethodImpl<Method>(std::move(method)) {
+    }
+  }; // struct Impl<Ret(Type::*)(_Args...) const>
+
+  std::shared_ptr<ImplBase> impl_;
+}; // class Function
 
 #include <iostream>
-#include <string>
+
+struct Foo {
+  Foo(int n) : member(n) {
+    std::cout << "this:" << this << std::endl;
+  }
+
+  int method(int a) {
+    std::cout << "method a:" << a << " this:" << this << std::endl;
+    return member;
+  }
+
+  int const_method(const char *a) const {
+    std::cout << "const_method a:" << a << " this:" << this << std::endl;
+    return member;
+  }
+
+  int member;
+};
+
+void test_method() {
+  std::cout << "enter " << __func__ << std::endl;
+  Function<int(Foo &, int)> f(&Foo::method);
+  Foo foo(5566);
+  std::cout << f(foo, 2498) << std::endl;
+  std::cout << "leave " << __func__ << std::endl;
+}
+
+void test_const_method() {
+  std::cout << "enter " << __func__ << std::endl;
+  Function<int(Foo *, const char *)> f(&Foo::const_method);
+  Foo foo(5566);
+  std::cout << f(&foo, "Hello, World!") << std::endl;
+  std::cout << "leave " << __func__ << std::endl;
+}
+
+void test() {
+  std::cout << "enter " << __func__ << std::endl;
+  Function<void()> func(&test_const_method);
+  func();
+
+  Function<int(int)> lambda = [&lambda] (int n) -> int {
+    return ((n == 1) ? 1 : (n * lambda(n - 1)));
+  };
+  std::cout << lambda(5) << std::endl;
+  std::cout << "leave " << __func__ << std::endl;
+}
 
 int main() {
-  Thread t([] (int a, const std::string &b, bool c) {
-    std::cout << "a:" << a << " b:" << b << " c:" << std::boolalpha << c << std::endl;
-  }, 5566, "Hello, World!", false);
+  test_method();
+  //test_const_method();
+  test();
   return 0;
 }
